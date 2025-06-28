ea34ce28788c314ca2010bd2273cc6abff4e590e
atharv3344 <atharv334455@gmail.com>
2025-06-28 11:26:59 +0530

    Update client.js

--- client.js
+++ client.js
@@ -5,3 +5,30 @@ function openPuzzle() { alert("Puzzle Mode Loading..."); }
 function openTournament() { alert("Tournament Feature Starting..."); }
 function openCoach() { alert("Coach Mode Initiated"); }
 function openLearn() { alert("Learning Mode"); }
+document.querySelector(".register-button").addEventListener("click", async () => {
+  const username = document.querySelector("input[type='text']").value;
+  const password = document.querySelector("input[type='password']").value;
+
+  const res = await fetch("/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+  alert(data.message);
+});
+
+document.querySelector(".login-button").addEventListener("click", async () => {
+  const username = document.querySelector("input[type='text']").value;
+  const password = document.querySelector("input[type='password']").value;
+
+  const res = await fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+  alert(data.message);
+});
